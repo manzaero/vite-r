2b1e096b090ec91ca485c1359b868dcee4abc9ed
manzaero <ligoero@gmail.com>
2024-09-26 20:31:32 +0300

    refact current year & code style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -28,8 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <div className="date-class"><p>It's {new Date().getFullYear()} now.</p></div>
     </>
   )
 }
-
-export default App
